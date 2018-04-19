279c34d5a8ddaf3ba62da95755034955adf69815
gully <igully@gmail.com>
2018-04-19 14:43:26 -0700

    Minor tweaks to DFM pull request

--- least_squares_cpu.py
+++ least_squares_cpu.py
@@ -23,9 +23,9 @@ yerr_hom = 0.4*np.ones(N)
 hom_cov = np.diag(yerr_hom ** 2)
 iid_cov = np.diag(yerr ** 2)
 true_cov = 0.5 * np.exp(-0.5 * (x[:, None]-x[None, :])**2 / 1.3**2) + np.diag(yerr ** 2)
-#y = np.random.multivariate_normal(y, true_cov)
+y_noised = np.random.multivariate_normal(y, true_cov)
 #np.save('y_fake.npy', y)
-y = np.load('y_fake.npy')
+#y = np.load('y_fake.npy')
 
 #Linear algebra
 A = np.vander(x, 2)
@@ -33,13 +33,10 @@ AT= A.T
 C = iid_cov
 
 t0 = time.time()
-# C_inv = np.linalg.inv(C)
-# S_inv = np.dot( np.dot(AT, C_inv), A)
-# S= np.linalg.inv(S_inv)
-# ls_m, ls_b = np.dot(np.dot( np.dot(S, A.T), C_inv), y)
 factor = cho_factor(C, overwrite_a=True)
 S_inv = np.dot(AT, cho_solve(factor, A))
-ls_m, ls_b = np.linalg.solve(S_inv, np.dot(A.T, cho_solve(factor, y)))
+S = np.linalg.inv(S_inv)
+ls_m, ls_b = np.dot(S, np.dot(AT, cho_solve(factor, y_noised)))
 t1 = time.time()
 
 net_time = t1-t0
